cc94aad1169086512e4495cb317adf0c094aef31
shuaibu78 <freezzemx@gmail.com>
2020-08-19 13:10:12 +0100

    completed the vendor's page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// fixed navbar
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
         document.getElementById("nav").classList.add("nav-color")
@@ -6,12 +7,36 @@ window.addEventListener("scroll", () => {
     }
 })
 
-function openNav() {
-    document.getElementById("myNav").style.width = "80%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+// Navigation bar
+const menuBtn = document.querySelector(".menu-icon span");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const xBtn = document.querySelector(".xnav");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector("form");
 
-  document.getElementById('copyyear').appendChild(document.createTextNode(new Date().getFullYear()))
+menuBtn.onclick = ()=>{
+    items.classList.add("active");
+    menuBtn.classList.add("hide");
+    searchBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+}
+
+cancelBtn.onclick = ()=>{
+    items.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    searchBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show");
+    form.classList.remove("active");
+    cancelBtn.style.color = "#ff3d00";
+}
+
+searchBtn.onclick = ()=>{
+    form.classList.add("active");
+    searchBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+}
+
+// Copyright
+let copyRightYear = document.getElementById('copyyear')
+copyRightYear.appendChild(document.createTextNode(new Date().getFullYear()))
