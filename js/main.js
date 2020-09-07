093925c4a36c97d9f5f105870f79c6319d4a367a
shuaibu muhammad <devshuaib@gmail.com>
2020-09-07 21:21:15 +0100

    completed the approve nd privacy page

--- js/main.js
+++ js/main.js
@@ -40,3 +40,59 @@ searchBtn.onclick = ()=>{
 // Copyright
 let copyRightYear = document.getElementById('copyyear')
 copyRightYear.appendChild(document.createTextNode(new Date().getFullYear()))
+
+
+// tabchanges
+const openTab = (evt, evtName) => {
+    let i, tabcontent, tablink;
+  
+    tabcontent = document.getElementsByClassName("approvedAgents");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablink.length; i++) {
+      tablink[i].className = tablink[i].className.replace("activeTab", "");
+    }
+  
+    document.getElementById(evtName).style.display = "flex";
+    evt.currentTarget.className += "activeTab";
+}
+
+const ClickedTab = (evt, evtName) => {
+    let i, tabcontent, tablink;
+  
+    tabcontent = document.getElementsByClassName("faq-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablink = document.getElementsByClassName("faqLinks");
+    for (i = 0; i < tablink.length; i++) {
+      tablink[i].className = tablink[i].className.replace("faqActive", "");
+    }
+  
+    document.getElementById(evtName).style.display = "grid";
+    evt.currentTarget.className += "faqActive";
+}
+document.getElementById("defaultOpen").click();
+
+const readMore = () => {
+    let more = document.getElementById ("more");
+    let hide = document.getElementById("opaque");
+    let more1 = document.getElementById("more1");
+    let hide1 = document.getElementById("opaque1");
+  
+    if (hide.style.display === "none") {
+      hide.style.display = "block";
+      hide1.style.display = "block";
+      more.style.display = "none";
+      more1.style.display = "none";
+    } else {
+      hide.style.display = "none";
+      hide1.style.display = "none";
+      more.style.display = "grid";
+      more1.style.display = "grid";
+    }
+  } 
